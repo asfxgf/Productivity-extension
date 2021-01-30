152d3bc563c5b36b695f1fc469e78f79262f8cc5
quentin deleglise <deleglise.quentin@hotmail.fr>
2021-01-31 00:31:10 +0100

    App v1 terminée

--- content.js
+++ content.js
@@ -1,19 +1,37 @@
 //alert("Hey boy");
 
-document.body.onload = addElement;
-
-
-function addElement () {
-  // crée un nouvel élément div
-  var referenceNode = document.querySelector('#SIvCob');
+document.body.onload = function CreateH1() {
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute("id","agediv")
   var newH1 = document.createElement("h1");
-  newH1.setAttribute("id", "age");
-  // et lui donne un peu de contenu
-  var newContent = document.createTextNode('Tu as 22 ans');
-  // ajoute le nœud texte au nouveau div créé
-  newH1.appendChild(newContent);
+  newH1.setAttribute("id","age");
+  var referenceNode = document.querySelector('#SIvCob');
+  referenceNode.after(newDiv);
+  newDiv.appendChild(newH1);
+  setInterval(addQuoteToGoogle, 150);
+};
 
-  // ajoute le nouvel élément créé et son contenu dans le DOM
-  var currentH1 = document.getElementById('h1');
-  referenceNode.after(newH1);
-}
+
+
+
+
+
+function computeExactAge() {
+
+  const birthday = new Date("December 04 1998");
+  const now = new Date();
+  var myAge = now - birthday;
+  var myExactAge = myAge/31556926000;
+  console.log("Temps à ma naissance : " + birthday.getTime());
+  console.log("Temps aujourd'hui : " + now.getTime());
+  console.log("mon age exacte : " + myExactAge);
+  return myExactAge;
+};
+
+
+  function addQuoteToGoogle() {
+    // Calcul l'age que j'ai en année + seconds
+      var exactAge = computeExactAge();
+      theH1 = document.getElementById("age");
+      theH1.innerHTML = 'Age : ' + exactAge.toFixed(9);
+  };
