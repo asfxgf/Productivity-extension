20bff64cc5d118b77e61b0511ccd42eded6566e5
quentin deleglise <deleglise.quentin@hotmail.fr>
2021-02-01 15:36:43 +0100

    quelques changements

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 //content.js
 
 document.body.onload = function CreateH1() {
-  var newDiv = document.createElement("div");
+  const newDiv = document.createElement("div");
   newDiv.setAttribute("id","agediv")
-  var newH1 = document.createElement("h1");
+  const newH1 = document.createElement("h1");
   newH1.setAttribute("id","age");
-  var referenceNode = document.querySelector('#SIvCob');
+  const referenceNode = document.querySelector('#SIvCob');
   referenceNode.after(newDiv);
   newDiv.appendChild(newH1);
   setInterval(addQuoteToGoogle, 80);
@@ -15,8 +15,8 @@ function computeExactAge() {
 
   const birthday = new Date("December 04 1998");
   const now = new Date();
-  var myAge = now - birthday;
-  var myExactAge = myAge/31556926000;
+  let myAge = now - birthday;
+  let myExactAge = myAge/31556926000;
   console.log("Temps à ma naissance : " + birthday.getTime());
   console.log("Temps aujourd'hui : " + now.getTime());
   console.log("mon age exacte : " + myExactAge);
@@ -26,7 +26,7 @@ function computeExactAge() {
 
   function addQuoteToGoogle() {
     // Calcul l'age que j'ai en année + seconds
-      var exactAge = computeExactAge();
+      const exactAge = computeExactAge();
       theH1 = document.getElementById("age");
       theH1.innerHTML = 'Age : ' + exactAge.toFixed(9);
   };
