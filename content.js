4cbea73ec912baff48488915640f2e2b0a791cd1
quentin deleglise <deleglise.quentin@hotmail.fr>
2021-11-29 01:11:43 +0100

    ajout de requêtes Api

--- content.js
+++ content.js
@@ -3,12 +3,22 @@
 document.body.onload = function CreateH1() {
   const newDiv = document.createElement("div");
   newDiv.setAttribute("id","agediv")
-  const newH1 = document.createElement("h1");
-  newH1.setAttribute("id","age");
+  const newAge = document.createElement("h1");
+  newAge.setAttribute("id","age");
+  const newActivity = document.createElement("h1");
+  newActivity.setAttribute("id","activity");
+  const newBitcoin = document.createElement("h1");
+  newBitcoin.setAttribute("id","bitcoin");
   const referenceNode = document.querySelector('#SIvCob');
   referenceNode.after(newDiv);
-  newDiv.appendChild(newH1);
+  newDiv.appendChild(newAge);
+  newDiv.appendChild(newBitcoin);
+  newDiv.appendChild(newActivity);
+
   setInterval(addQuoteToGoogle, 80);
+  httpGet("https://api.coindesk.com/v1/bpi/currentprice.json");
+  
+  httpGet("https://www.boredapi.com/api/activity");
 };
 
 function computeExactAge() {
@@ -17,16 +27,38 @@ function computeExactAge() {
   const now = new Date();
   let myAge = now - birthday;
   let myExactAge = myAge/31556926000;
-  console.log("Temps à ma naissance : " + birthday.getTime());
-  console.log("Temps aujourd'hui : " + now.getTime());
-  console.log("mon age exacte : " + myExactAge);
+  //console.log("Temps à ma naissance : " + birthday.getTime());
+  //console.log("Temps aujourd'hui : " + now.getTime());
+  //console.log("mon age exacte : " + myExactAge);
   return myExactAge;
 };
 
+function addQuoteToGoogle() {
+  // Calcul l'age que j'ai en année + seconds
+    const exactAge = computeExactAge();
+    theH1 = document.getElementById("age");
+    theH1.innerHTML = 'Age : ' + exactAge.toFixed(9);
+};
 
-  function addQuoteToGoogle() {
-    // Calcul l'age que j'ai en année + seconds
-      const exactAge = computeExactAge();
-      theH1 = document.getElementById("age");
-      theH1.innerHTML = 'Age : ' + exactAge.toFixed(9);
-  };
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    response = xmlHttp.responseText;
+    response_obj = JSON.parse(response);
+    if (theUrl == "https://api.coindesk.com/v1/bpi/currentprice.json") {
+      theBitcoin = document.getElementById("bitcoin");
+      theBitcoin.innerHTML = "Prix actuel du BITCOIN : " + response_obj.bpi.EUR.rate + " €";
+      return console.log("Prix actuel du BITCOIN : " + response_obj.bpi.EUR.rate + " €");
+    } else if (theUrl == "https://www.boredapi.com/api/activity") {
+      //console.log(response_obj["activity"]);
+      theActivity = document.getElementById("activity");
+      theActivity.innerHTML = "Activity Idea : " + response_obj["activity"];
+      return console.log("Activity Idea : " + response_obj["activity"])
+    } else {
+      return console.log(response);
+    }
+}
+
+
+// alert("bim")
